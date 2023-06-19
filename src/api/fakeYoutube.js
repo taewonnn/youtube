6846b3a0ce83dfdce0b924a3be812e6af5f1c56d
TaeWon Park <thisis_tw94@naver.com>
2023-06-19 20:29:46 +0900

    mock server - class

--- src/api/fakeYoutube.js
+++ src/api/fakeYoutube.js
@@ -1,12 +1,28 @@
 import axios from "axios";
 
-export async function search(keyword) {
-  return axios
-    .get(`/videos/${keyword ? 'search' : 'popular'}.json`)
-    .then((res) => {
-      console.log(res);
-      return res.data.items;
-    })
+
+
+
+export default class FakeYoutube {
+  constructor() {
+
+  }
+  async search(keyword) {
+    return keyword ? this.#searchByKeyword(keyword) : this.#mostPopular();
+  }
+
+  async #searchByKeyword() {
+    return axios
+      .get(`/videos/search.json`)
+      .then(res => res.data.items)
+      .then(items => item.map(item => ({...item, id: item.id.videoId})))
+  }
+
+  async #mostPopular() {
+    return axios
+      .get(`/videos/popular.json`)
+      .then(res => res.data.items)
+  }
 }
 
 
