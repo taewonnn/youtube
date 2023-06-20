1f808cd331aca787fbaaa7bc84c8c6c3adad4c2c
TaeWon Park <thisis_tw94@naver.com>
2023-06-20 21:01:28 +0900

    apiClient 공통화

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,11 +1,7 @@
-import axios from "axios";
 
 export default class Youtube {
-  constructor() {
-    this.httpClient = axios.create({
-      baseURL: 'https://youtube.googleapis.com/youtube/v3',
-      params: { key: process.env.REACT_APP_YOUTUBE_API_KEY }
-    })
+  constructor(apiClient) {
+    this.apiClient = apiClient;
   }
 
   async search(keyword) {
@@ -13,25 +9,29 @@ export default class Youtube {
   }
 
   async #searchByKeyword(keyword) {
-    return this.httpClient
-      .get('search', {params: {
+    return this.apiClient
+      .search({
+      params: {
         part: 'snippet',
         maxResults: 25,
         type: 'video',
         q: keyword,
-        }})
+      },
+    })
       .then(res => res.data.items)
       .then((items) => items.map(item => ({...item, id: item.id.videoId})))
   }
 
   async #mostPopular() {
-    return this.httpClient
-      .get('videos', {params: {
-          part: 'snippet',
-          maxResults: 25,
-          type: 'video',
-          chart: 'mostPopular',
-        }})
+    return this.apiClient
+      .videos({
+      params: {
+        part: 'snippet',
+        maxResults: 25,
+        type: 'video',
+        chart: 'mostPopular',
+      }
+    })
       .then(res => res.data.items)
       .then((items) => items.map(item => ({...item, id: item.id.videoId})))
   }
