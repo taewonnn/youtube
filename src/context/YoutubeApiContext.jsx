ca85d333829e6f3b779bf8ae8a96f0ab01ad9ad2
TaeWon Park <thisis_tw94@naver.com>
2023-06-20 20:34:13 +0900

    api context

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -2,3 +2,9 @@ import {createContext} from "react";
 
 export const YoutubeApiContext = createContext();
 
+
+export function YoutubeApiProvider({children}) {
+  return (
+    <YoutubeApiProvider value={{youtube}}>{children}</YoutubeApiProvider>
+  )
+}
