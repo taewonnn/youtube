ae9a0b39757d310b6abb27092c81cd30ba630b05
TaeWon Park <thisis_tw94@naver.com>
2023-06-21 20:25:24 +0900

    의존성 추가 - timeago

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,10 +1,20 @@
+import { format, register } from "timeago.js";
+import koLocale from 'timeago.js/lib/lang/ko'
+
+register('ko', koLocale);
+
 function VideoCard ({ video }) {
 
-  const {title, thumbnails} = video.snippet
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet
 
   return (
     <li>
       <img src={thumbnails.medium.url} alt={title} />
+      <div>
+        <p>{title}</p>
+        <p>{channelTitle}</p>
+        <p>{format(publishedAt, 'ko')}</p>
+      </div>
     </li>
   )
 }
