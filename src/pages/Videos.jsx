39f8caa362b0bf1eb60ebc9342795fee53a22a40
TaeWon Park <thisis_tw94@naver.com>
2023-06-21 20:30:57 +0900

    videos style

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -28,7 +28,7 @@ function Videos () {
         {keyword ? `${keyword}` : 'Hot Trend'}
         {isLoading && <p>Loading...</p>}
         {error && <p>Something is wrong</p>}
-        {videos && <ul>
+        {videos && <ul className='grid grid-cols-1 sm:grid-cols-2 lg: grid-cols-3 xl: grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-6'>
           { videos.map((video) => <VideoCard key={video.id} video={video}/>) }
         </ul>}
       </div>
