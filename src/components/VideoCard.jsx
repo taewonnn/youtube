0d753de8196e9ca9c72bd1db02a470c9fd03c58d
TaeWon Park <thisis_tw94@naver.com>
2023-06-22 22:14:53 +0900

    videoDetail

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,15 +1,21 @@
 import {formatAgo} from "../util/datae";
+import {useNavigate} from "react-router-dom";
 
-function VideoCard ({ video }) {
+function VideoCard({video}) {
 
-  const { title, thumbnails, channelTitle, publishedAt } = video.snippet
+  const {title, thumbnails, channelTitle, publishedAt} = video.snippet
+
+  // useNavigate
+  const navigate = useNavigate();
 
   return (
-    <li>
+    <li onClick={() => {
+      navigate(`videos/watch/${video.id}`, {state: {video}})
+    }}>
       <img
         className='w-full'
         src={thumbnails.medium.url}
-        alt={title} />
+        alt={title}/>
       <div>
         <p className='font-bold my-2 line-clamp-2'>{title}</p>
         <p className='text-sm opacity-80'>{channelTitle}</p>
