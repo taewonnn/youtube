93e0c319e60260b90566c6718643aeb79e1d6c88
TaeWon Park <thisis_tw94@naver.com>
2023-06-22 22:23:55 +0900

    videoDetail

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,10 +1,32 @@
-function VideoDetail () {
+import { useLocation } from 'react-router-dom';
+import ChannelInfo from '../components/ChannelInfo';
+import RelatedVideos from '../components/RelatedVideos';
+
+export default function VideoDetail() {
+  const {
+    state: { video },
+  } = useLocation();
+  const { title, channelId, channelTitle, description } = video.snippet;
   return (
-    <div>
-      VideoDetail
-    </div>
-  )
+    <section>
+      <article>
+        <iframe
+          id='player'
+          type='text/html'
+          width='100%'
+          height='640'
+          src={`http://www.youtube.com/embed/${video.id}`}
+          frameBorder='0'
+        />
+        <div>
+          <h2>{title}</h2>
+          <ChannelInfo id={channelId} name={channelTitle} />
+          <pre>{description}</pre>
+        </div>
+      </article>
+      <section>
+        <RelatedVideos id={video.id} />
+      </section>
+    </section>
+  );
 }
-
-export default VideoDetail;
-
