8bad45f31234c3d5098b4d7e11a0037fc427253a
TaeWon Park <thisis_tw94@naver.com>
2023-06-24 00:56:13 +0900

    RelatedVideos api

--- src/components/RelatedVideos.jsx
+++ src/components/RelatedVideos.jsx
@@ -1,4 +1,11 @@
+import { useYoutubeApi } from "../context/YoutubeApiContext";
+import { useQuery } from "@tanstack/react-query";
+
 function RelatedVideos ({ id }) {
+
+  const { youtube } = useYoutubeApi();
+  const { error, isLoading, videos } = useQuery(['related', id], () => youtube.relatedVideos(id));
+
   return (
     <div>
       {id}
