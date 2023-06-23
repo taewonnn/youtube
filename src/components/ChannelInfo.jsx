885066ea38d12d3764ebd172704bfd150cc9a336
TaeWon Park <thisis_tw94@naver.com>
2023-06-23 21:22:40 +0900

    ChannelImageURL api

--- src/components/ChannelInfo.jsx
+++ src/components/ChannelInfo.jsx
@@ -1,7 +1,15 @@
-function ChannelInfo ({ id, name }) {
+import { useYoutubeApi } from "../context/YoutubeApiContext";
+import { useQuery } from "@tanstack/react-query";
+
+function ChannelInfo({id, name}) {
+
+  const { youtube } = useYoutubeApi();
+  const { error, isLoading, data: url } = useQuery(['channel', id], () => youtube.ChannelImageURL(id));
+
   return (
     <div>
-      {name}
+      {url && <img src={url} alt={name} /> }
+      <p>{name}</p>
     </div>
   )
 }
