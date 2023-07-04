579732f011bb7f4e31ae4f4f69c33236b60123a2
TaeWon Park <thisis_tw94@naver.com>
2023-07-04 21:08:03 +0900

    https 수정

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -18,7 +18,7 @@ export default function VideoDetail() {
           width='100%'
           height='640'
           title={title}
-          src={`http://www.youtube.com/embed/${video.id}`}
+          src={`https://www.youtube.com/embed/${video.id}`}
         />
         <div className='p-8'>
           <h2 className='text-xl font-bold'>{title}</h2>
